99ce9219399e80d693493f4b1af268b8aab224fe
João Pedro <135559991+joaoxddx@users.noreply.github.com>
2026-06-05 19:18:38 -0300

    fix: corrige rotas e assets para deploy no github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { AppRouter } from './routers/app.routers';
 
 function App() {
